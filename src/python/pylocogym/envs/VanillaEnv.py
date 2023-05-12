1162d44233351c5093b9527fa4636fcddf993f2f
Alex-Marshall-Master <hujian@ethz.ch>
2023-05-12 20:51:26 +0200

    Naive version of training env

--- src/python/pylocogym/envs/VanillaEnv.py
+++ src/python/pylocogym/envs/VanillaEnv.py
@@ -13,102 +13,10 @@ sys.modules["module.name"] = pyloco
 spec.loader.exec_module(pyloco)
 
 
-from scipy.spatial.transform import Rotation as R
+from pylocogym.envs.rewards.bob.humanoid_reward import Reward
+from pylocogym.data.deep_mimic_motion import DeepMimicMotion
+from pylocogym.data.lerp_dataset import LerpMotionDataset
 
-class Retarget:
-    def __init__(self, action_shape, joint_lower_limit, joint_upper_limit, joint_default_angle = None):
-
-        self.joint_angle_limit_low = joint_lower_limit
-        self.joint_angle_limit_high = joint_upper_limit
-        self.joint_angle_default = joint_default_angle
-        if self.joint_angle_default is None:
-            self.joint_angle_default = np.array([ 0.  ,  0.  ,  0.  ,  0.  ,  0.  ,  0.  ,  0.  ,  0.  ,  0.  ,
-                                                0.  ,  0.  ,  0.  ,  0.  ,  0.  ,  0.  ,  0.  ,  0.  ,  0.  ,
-                                                0.  ,  0.  ,  0.  ,  0.  ,  0.  ,  0.  ,  0.  ,  0.  ,  0.  ,
-                                                0.  ,  0.  ,  0.  ,  0.  ,  0.  ,  0.  ,  0.  ,  0.  ,  0.  ,
-                                                0.  ,  0.  ,  0.  ,  0.  ,  0.  ,  0.  ,  0.  ,  0 ])
-        self.joint_scale_factors = np.maximum(abs(self.joint_angle_default - self.joint_angle_limit_low),
-                                abs(self.joint_angle_default - self.joint_angle_limit_high))
-        self.action_shape = action_shape
-        
-    def quart_to_rpy(self, q, mode):
-        # q is in (w,x,y,z) format
-        q_xyzw = list(q[1:])
-        q_xyzw.append(q[0])
-        r = R.from_quat(q_xyzw) 
-        euler = r.as_euler(mode)
-        return euler[0], euler[1], euler[2]
-    
-    def rescale_action(self, action):
-        bound_action = np.minimum(np.maximum(action,self.joint_angle_limit_low),self.joint_angle_limit_high)
-        scaled_action = (bound_action - self.joint_angle_default) / self.joint_scale_factors 
-        return scaled_action
-    
-    def retarget_joint_angle(self, motion_clips_q, require_root = False):
-        """Given a motion_clips orientation data, return a retarget action"""
-        action = np.zeros(self.action_shape)
-        if not require_root:
-
-            (chest_z, chest_y, chest_x) = self.quart_to_rpy(motion_clips_q[7:11], 'zyx')
-            (neck_z,  neck_y,  neck_x) = self.quart_to_rpy(motion_clips_q[11:15],'zyx')
-            (r_hip_z, r_hip_x, r_hip_y) = self.quart_to_rpy(motion_clips_q[15:19],'zxy')
-            (r_ankle_z, r_ankle_x, r_ankle_y) = self.quart_to_rpy(motion_clips_q[20:24],'zxy')
-            (r_shoulder_z, r_shoulder_x, r_shoulder_y) = self.quart_to_rpy(motion_clips_q[24:28],'zxy')
-            (l_hip_z, l_hip_x, l_hip_y) = self.quart_to_rpy(motion_clips_q[29:33],'zxy')
-            (l_ankle_z, l_ankle_x, l_ankle_y) = self.quart_to_rpy(motion_clips_q[34:38],'zxy')
-            (l_shoulder_z, l_shoulder_x, l_shoulder_y) = self.quart_to_rpy(motion_clips_q[38:42],'zxy')
-
-            # chest - xyz euler angle 
-            action[0] = -chest_z
-            action[3] = chest_y
-            action[6] = chest_x
-
-            # neck - xyz euler angle 
-            action[18] = -neck_z
-            action[23] = neck_y
-            action[26] = neck_x
-
-            # shoulder - xzy euler angle 
-            action[27] = -l_shoulder_z
-            action[30] = l_shoulder_x
-            action[33] = l_shoulder_y
-
-            action[28] = -r_shoulder_z
-            action[31] = r_shoulder_x
-            action[34] = r_shoulder_y
-
-            # ankle - xzy euler angle 
-            action[13] = -l_ankle_z
-            action[16] = l_ankle_x
-
-            action[14] = -r_ankle_z
-            action[17] = r_ankle_x            
-
-            # hip - xzy euler angle 
-            action[1] = -l_hip_z
-            action[4] = l_hip_x
-            action[7] = l_hip_y
-
-            action[2] = -r_hip_z
-            action[5] = r_hip_x
-            action[8] = r_hip_y
-
-            r_knee = motion_clips_q[19:20]
-            r_elbow = motion_clips_q[28:29]
-            l_knee = motion_clips_q[33:34]
-            l_elbow = motion_clips_q[42:43]
-
-            # elbow - revolute joint 
-            action[36] = l_elbow
-            action[37] = r_elbow
-
-            # knee - revolute joint 
-            action[10] = l_knee
-            action[11] = r_knee
-
-            action = self.rescale_action(action)
-
-        return action
 
 class VanillaEnv(PylocoEnv):
 
@@ -116,12 +24,12 @@ class VanillaEnv(PylocoEnv):
         sim_dt = 1.0 / env_params['simulation_rate']
         con_dt = 1.0 / env_params['control_rate']
 
-        if env_params['robot_model'] == "Dog":
-            robot_id = 0
-        elif env_params['robot_model'] == "Go1":
-            robot_id = 1
-        elif env_params['robot_model'] == "Bob":
-            robot_id = 2
+        # if env_params['robot_model'] == "Dog":
+        #     robot_id = 0
+        # elif env_params['robot_model'] == "Go1":
+        #     robot_id = 1
+        # elif env_params['robot_model'] == "Bob":
+        robot_id = 2
 
         loadVisuals = False
         super().__init__(pyloco.VanillaSimulator(sim_dt, con_dt, robot_id, loadVisuals), env_params, max_episode_steps)
@@ -131,23 +39,23 @@ class VanillaEnv(PylocoEnv):
         self.box_throwing_interval = 100
         self.box_throwing_strength = 2
         self.box_throwing_counter = 0
-
-        if "reward_file_path" in reward_params.keys():
-            reward_file_path = reward_params["reward_file_path"]
-
-            self.reward_utils = SourceFileLoader('reward_utils', reward_file_path).load_module()
-        else:
-            raise Exception("Reward file not specified. Please specify via --rewardFile.")
-
+        
         self.cnt_timestep_size = self._sim.control_timestep_size  # this should be equal to con_dt
         self.current_step = 0
-        self.max_episode_steps = max_episode_steps
+        # self.max_episode_steps = max_episode_steps
         self.reward_params = reward_params
         self.sum_episode_reward_terms = {}
         self.action_buffer = np.zeros(self.num_joints * 3)  # history of actions [current, previous, past previous]
 
+        self.reward_utils = Reward(self.cnt_timestep_size, self.num_joints, 
+                self.joint_angle_limit_low, self.joint_angle_limit_high, reward_params)
+
         self.rng = np.random.default_rng(
             env_params.get("seed", 1))  # create a local random number generator with seed
+        
+        self.dataset = DeepMimicMotion(reward_params['reward_file_path'],0, "none")
+        self.max_episode_steps = int(self.dataset.duration / self.cnt_timestep_size) - 1
+        self.lerp = LerpMotionDataset(self.dataset)
 
     def reset(self, seed=None, return_info=False, options=None):
         # super().reset(seed=seed)  # We need this line to seed self.np_random
@@ -158,7 +66,7 @@ class VanillaEnv(PylocoEnv):
         self.sum_episode_reward_terms = {}
         self.action_buffer = np.concatenate(
             (self.joint_angle_default, self.joint_angle_default, self.joint_angle_default), axis=None)
-
+        self.lerp.reset()
         info = {"msg": "===Episode Reset Done!===\n"}
         return (observation, info) if return_info else observation
 
@@ -181,11 +89,8 @@ class VanillaEnv(PylocoEnv):
         self.action_buffer[0:self.num_joints] = action_applied
 
         # compute reward
-        reward, reward_info = self.reward_utils.compute_reward(observation, self.cnt_timestep_size, self.num_joints,
-                                                               self.reward_params, self.get_feet_status(),
-                                                               self._sim.get_all_motor_torques(), self.action_buffer,
-                                                               self.is_obs_fullstate, self.joint_angle_default,
-                                                               self._sim.nominal_base_height)
+        reward, reward_info = self.reward_utils.compute_reward(observation, self._sim.get_all_motor_torques(), self.action_buffer,
+                                                               self.is_obs_fullstate, self._sim.nominal_base_height, self.lerp)
 
         self.sum_episode_reward_terms = {key: self.sum_episode_reward_terms.get(key, 0) + reward_info.get(key, 0) for
                                          key in reward_info.keys()}
@@ -196,7 +101,7 @@ class VanillaEnv(PylocoEnv):
 
         # punishment for early termination
         if terminated:
-            reward -= self.reward_utils.punishment(self.current_step, self.max_episode_steps, self.reward_params)
+            reward -= self.reward_utils.punishment(self.current_step, self.max_episode_steps)
 
         info = {
             "is_success": truncated,
--- src/python/pylocogym/envs/rewards/bob/humanoid_reward.py
+++ src/python/pylocogym/envs/rewards/bob/humanoid_reward.py
@@ -2,83 +2,124 @@
 Computing reward for Vanilla setup, constant target speed, gaussian kernels
 """
 import numpy as np
-from numpy.linalg import norm
 from pylocogym.envs.rewards.utils.utils import *
+from pylocogym.envs.rewards.bob.humanoid_retarget import Retarget
 
-import pickle
-import time
+class Reward:
+    def __init__(self, cnt_timestep, num_joints, joint_lower_limit, joint_higher_limit, reward_params):
+        """
+        :variable dt: control time step size
+        :variable num_joints: number of joints
+        :variable params: reward params read from the config file
+        """
+        self.dt = cnt_timestep
+        self.num_joints = num_joints
+        self.params = reward_params
+        self.retarget = Retarget(num_joints,joint_lower_limit,joint_higher_limit)
+        
+
+    def compute_reward(self, observation_raw, all_torques, action_buffer, is_obs_fullstate,
+                nominal_base_height, dataloader):
+        """
+        Compute the reward based on observation (Vanilla Environment).
+
+        :param observation_raw: current observation
+        :param all_torques: torque records during the last control timestep
+        :param action_buffer: history of previous actions
+        :param is_obs_fullstate: flag to choose full state obs or not.
+        
+        :return: total reward, reward information (different terms can be passed here to be plotted in the graphs)
+        """
+
+        # test_data = {'observation_raw': observation_raw, 'dt': dt, 'num_joints': num_joints, 'params': params,
+        #              'feet_status': feet_status, 'all_torques': all_torques, 'action_buffer': action_buffer,
+        #              'is_obs_fullstate': is_obs_fullstate, 'joint_angles_default': joint_angles_default,
+        #              'nominal_base_height': nominal_base_height}
+        
+        num_joints = self.num_joints
+        dt = self.dt
+        params = self.params
+
+        observation = ObservationData(observation_raw, num_joints, is_obs_fullstate)
+
+        action_dot, action_ddot = calc_derivatives(action_buffer, dt, num_joints)
+        cmd_fwd_vel = params.get("fwd_vel_cmd", 1.0)
+        torque = tail(all_torques, num_joints)
+
+        now_t = observation.time_stamp
+        
+        # =============
+        # define cost/reward terms here:
+        # =============
+
+        # x&z position corresponds
+        # MOTION CLIPS : X FORWARD, Z RIGHT, Y UP
+        # OURS MODEL: Z FORWARD, X LEFT, Y UP
+
+        if dataloader.eval(now_t) is None:
+            print(now_t)
+        desired_base_pos_x = dataloader.eval(now_t).q[0]/dataloader.eval(now_t).q[1]
+        now_base_z = observation.pos[2]/observation.pos[1]
+        diff = np.linalg.norm(desired_base_pos_x - now_base_z)
+        sigma_vel = params.get("sigma_velocity", 0)
+        weight_vel = params.get("weight_velocity", 0)
+        forward_vel_reward = weight_vel * np.exp(-diff**2/(2.0*sigma_vel**2))
+
+        height = observation.y
+        diff_squere = (height - nominal_base_height)**2
+        sigma_height = params.get("sigma_height", 0)
+        weight_height = params.get("weight_height", 0)
+        height_reward = weight_height * np.exp(-diff_squere/(2.0*sigma_height**2))
+
+        roll_squere = (observation.roll)**2
+        pitch_squere = (observation.pitch)**2
+        weight_attitude = params.get("weight_attitude", 0)
+        sigma_attitude = params.get("sigma_attitude", 0)
+        attitude_reward = weight_attitude * np.exp(-(roll_squere + pitch_squere)/(4.0*sigma_attitude**2))
+
+        N = num_joints
+        weight_torque = params.get("weight_torque", 0)
+        sigma_torque = params.get("sigma_torque", 0)
+        torque_reward = weight_torque * np.exp(-np.sum(torque**2)/(2.0*N*sigma_torque**2))
+
+        weight_smoothness1 = params.get("weight_smoothness1", 0)
+        sigma_smoothness1 = params.get("sigma_smoothness1", 0)
+        smoothness1_reward = weight_smoothness1 * np.exp(-np.sum(action_dot**2)/(2.0*N*sigma_smoothness1**2))
+
+        weight_smoothness2 = params.get("weight_smoothness2", 0)
+        sigma_smoothness2 = params.get("sigma_smoothness2", 0)    
+        smoothness2_reward = weight_smoothness2 * np.exp(-np.sum(action_ddot**2)/(2.0*N*sigma_smoothness2**2))
+
+        # Motion imitation reward #
+        joint_angles = observation.joint_angles
+        desired_angles = self.retarget.retarget_joint_angle(dataloader.eval(now_t).q)
+        diff = joint_angles - desired_angles
+        weight_joints = params.get("weight_joints", 0)
+        sigma_joints = params.get("sigma_joints", 0)
+        joint_reward = weight_joints * np.exp(-np.sum(np.square(diff))/(2.0*N*sigma_joints**2))
+
+        # =============
+        # sum up rewards
+        # =============
+        smoothness_reward = params.get("weight_smoothness", 0) * (smoothness1_reward + smoothness2_reward)
+        reward = forward_vel_reward + smoothness_reward + torque_reward + height_reward + attitude_reward + joint_reward
+
+        info = {
+            "forward_vel_reward": forward_vel_reward,
+            "height_reward": height_reward,
+            "attitude_reward": attitude_reward,
+
+            "torque_reward": torque_reward,
+            "smoothness1_reward": smoothness1_reward,
+            "smoothness2_reward": smoothness2_reward,
+            "smoothness_reward": smoothness_reward,
+
+            "joint_reward": joint_reward
+        }
+
+        return reward, info
 
 
-def compute_reward(observation_raw, dt, num_joints, params, feet_status, all_torques, action_buffer, is_obs_fullstate,
-                   joint_angles_default, nominal_base_height):
-    """
-    Compute the reward based on observation (Vanilla Environment).
-
-    :param observation_raw: current observation
-    :param dt: control time step size
-    :param num_joints: number of joints
-    :param params: reward params read from the config file
-    :param feet_status: pos, vel and swing status of robot feet
-    :param all_torques: torque records during the last control timestep
-    :param action_buffer: history of previous actions
-    :param is_obs_fullstate: flag to choose full state obs or not.
-    :param joint_angles_default: default joint angles
-    :return: total reward, reward information (different terms can be passed here to be plotted in the graphs)
-    """
-
-    # test_data = {'observation_raw': observation_raw, 'dt': dt, 'num_joints': num_joints, 'params': params,
-    #              'feet_status': feet_status, 'all_torques': all_torques, 'action_buffer': action_buffer,
-    #              'is_obs_fullstate': is_obs_fullstate, 'joint_angles_default': joint_angles_default,
-    #              'nominal_base_height': nominal_base_height}
-
-    observation = ObservationData(observation_raw, num_joints, is_obs_fullstate)
-
-    action_dot, action_ddot = calc_derivatives(action_buffer, dt, num_joints)
-    cmd_fwd_vel = params.get("fwd_vel_cmd", 1.0)
-    torque = tail(all_torques, num_joints)
-
-    # =============
-    # define cost/reward terms here:
-    # =============
-    # TODO: Implement the rewards here.
-    # Hints:
-    # - Use function params.get("weight_velocity", 0) to get the value of parameters set in the .conf file.
-    forward_vel_reward = params.get("weight_velocity", 0) * 0
-
-    height_reward = 0
-
-    attitude_reward = 0
-
-    torque_reward = 0
-
-    smoothness1_reward = 0
-
-    smoothness2_reward = 0
-
-    joint_reward = 0
-
-    # =============
-    # sum up rewards
-    # =============
-    smoothness_reward = params.get("weight_smoothness", 0) * (smoothness1_reward + smoothness2_reward)
-    reward = forward_vel_reward + smoothness_reward + torque_reward + height_reward + attitude_reward + joint_reward
-
-    info = {
-        "forward_vel_reward": forward_vel_reward,
-        "height_reward": height_reward,
-        "attitude_reward": attitude_reward,
-
-        "torque_reward": torque_reward,
-        "smoothness1_reward": smoothness1_reward,
-        "smoothness2_reward": smoothness2_reward,
-        "smoothness_reward": smoothness_reward,
-
-        "joint_reward": joint_reward
-    }
-
-    return reward, info
-
-
-def punishment(current_step, max_episode_steps, params):  # punishment for early termination
-    penalty = params['weight_early_penalty'] * (max_episode_steps - current_step)
-    return penalty
+    def punishment(self, current_step, max_episode_steps):  # punishment for early termination
+        penalty = self.params['weight_early_penalty'] * (max_episode_steps - current_step)
+        return penalty
