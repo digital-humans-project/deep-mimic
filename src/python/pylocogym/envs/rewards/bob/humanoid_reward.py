a60254a78f52f40885e49a3e1da4131cb19e0ddf
Alex-Marshall-Master <hujian@ethz.ch>
2023-05-19 18:14:26 +0200

    Update Reward term
    1. deleted torque term
    2. separate mimic joints and those "useless joints", the latter one uses smoothness reward
    3. add joints velocity reward
    4. leg reward still waiting for the end effector reward to take the place of it

--- src/python/pylocogym/envs/rewards/bob/humanoid_reward.py
+++ src/python/pylocogym/envs/rewards/bob/humanoid_reward.py
@@ -3,10 +3,9 @@ Computing reward for Vanilla setup, constant target speed, gaussian kernels
 """
 import numpy as np
 from pylocogym.envs.rewards.utils.utils import *
-from pylocogym.envs.rewards.bob.humanoid_retarget import Retarget
 
 class Reward:
-    def __init__(self, cnt_timestep, num_joints, joint_lower_limit, joint_higher_limit, reward_params):
+    def __init__(self, cnt_timestep, num_joints, mimic_joints_index, reward_params):
         """
         :variable dt: control time step size
         :variable num_joints: number of joints
@@ -15,10 +14,10 @@ class Reward:
         self.dt = cnt_timestep
         self.num_joints = num_joints
         self.params = reward_params
-        self.retarget = Retarget(num_joints,joint_lower_limit,joint_higher_limit)
+        self.mimic_joints_index = mimic_joints_index
         
 
-    def compute_reward(self, observation_raw, all_torques, action_buffer, is_obs_fullstate,
+    def compute_reward(self, observation_raw, action_buffer, is_obs_fullstate,
                 nominal_base_height, dataloader, clips_play_speed):
         """
         Compute the reward based on observation (Vanilla Environment).
@@ -43,88 +42,104 @@ class Reward:
         observation = ObservationData(observation_raw, num_joints, is_obs_fullstate)
 
         action_dot, action_ddot = calc_derivatives(action_buffer, dt, num_joints)
-        torque = tail(all_torques, num_joints)
 
         # Accelerate or decelerate motion clips, usually deceleration
         # (clips_play_speed < 1 nomarlly)
         now_t = observation.time_stamp*clips_play_speed
         
-        # =============
-        # define cost/reward terms here:
-        # =============
+        # =======================
+        # MOTION CLIPS coordinate : X FORWARD, Z RIGHT, Y UP
+        # OURS MODEL coordinate   : Z FORWARD, X LEFT, Y UP
+        # =======================
 
-        # x&z position corresponds
-        # MOTION CLIPS : X FORWARD, Z RIGHT, Y UP
-        # OURS MODEL: Z FORWARD, X LEFT, Y UP
-
-        desired_base_pos_x = dataloader.eval(now_t).q[0]/dataloader.eval(now_t).q[1]
-        now_base_z = observation.pos[2]/observation.pos[1]
+        # Forward root position reward
+        sample = dataloader.eval(now_t)
+        desired_base_pos_x = sample.q[0]
+        now_base_z = observation.pos[2]
         diff = np.linalg.norm(desired_base_pos_x - now_base_z)
         sigma_vel = params.get("sigma_velocity", 0)
         weight_vel = params.get("weight_velocity", 0)
         forward_vel_reward = weight_vel * np.exp(-diff**2/(2.0*sigma_vel**2))
 
+        # Root height reward
         height = observation.y
         diff_squere = (height - nominal_base_height)**2
         sigma_height = params.get("sigma_height", 0)
         weight_height = params.get("weight_height", 0)
         height_reward = weight_height * np.exp(-diff_squere/(2.0*sigma_height**2))
 
-        (desired_yaw, desired_pitch, desired_roll) = self.retarget.retarget_base_orientation(dataloader.eval(now_t).q)
+        # Root orientation reward
+        (desired_yaw, desired_pitch, desired_roll) = sample.q[3:6]
         roll_squere = (observation.roll - desired_roll)**2
         pitch_squere = (observation.pitch - desired_pitch)**2
         yaw_squere = (observation.yaw - desired_yaw)**2
-        weight_attitude = params.get("weight_attitude", 0)
-        sigma_attitude = params.get("sigma_attitude", 0)
-        attitude_reward = weight_attitude * np.exp(-(roll_squere + pitch_squere + yaw_squere)/(6.0*sigma_attitude**2))
+        weight_root_ori = params.get("weight_root_ori", 0)
+        sigma_root_ori = params.get("sigma_root_ori", 0)
+        root_ori_reward = weight_root_ori * np.exp(-(roll_squere + pitch_squere + yaw_squere)/(6.0*sigma_root_ori**2))
 
         N = num_joints
-        weight_torque = params.get("weight_torque", 0)
-        sigma_torque = params.get("sigma_torque", 0)
-        torque_reward = weight_torque * np.exp(-np.sum(torque**2)/(2.0*N*sigma_torque**2))
+        N_mimic = len(self.mimic_joints_index)
+        set_other = set(range(N)) - self.mimic_joints_index # joints that have no corresponding in motion clips
 
+        # Unused joints smoothness reward
+        rest_joints_dot = action_dot[list(set_other)]
         weight_smoothness1 = params.get("weight_smoothness1", 0)
         sigma_smoothness1 = params.get("sigma_smoothness1", 0)
-        smoothness1_reward = weight_smoothness1 * np.exp(-np.sum(action_dot**2)/(2.0*N*sigma_smoothness1**2))
+        smoothness1_reward = weight_smoothness1 * np.exp(-np.sum(rest_joints_dot**2)/(2.0*(N-N_mimic)*sigma_smoothness1**2))
 
+        rest_joints_ddot = action_ddot[list(set_other)]
         weight_smoothness2 = params.get("weight_smoothness2", 0)
         sigma_smoothness2 = params.get("sigma_smoothness2", 0)    
-        smoothness2_reward = weight_smoothness2 * np.exp(-np.sum(action_ddot**2)/(2.0*N*sigma_smoothness2**2))
+        smoothness2_reward = weight_smoothness2 * np.exp(-np.sum(rest_joints_ddot**2)/(2.0*(N-N_mimic)*sigma_smoothness2**2))
 
         # Motion imitation reward 
-        joint_angles = observation.joint_angles
-        desired_angles = self.retarget.retarget_joint_angle(dataloader.eval(now_t).q)
+        joint_angles = observation.joint_angles[list(self.mimic_joints_index)]
+        desired_angles = sample.q[list(self.mimic_joints_index)]
         diff = joint_angles - desired_angles
         weight_joints = params.get("weight_joints", 0)
         sigma_joints = params.get("sigma_joints", 0)
-        joint_reward = weight_joints * np.exp(-np.sum(np.square(diff))/(2.0*N*sigma_joints**2))
+        joints_reward = weight_joints * np.exp(-np.sum(np.square(diff))/(2.0*N_mimic*sigma_joints**2))
 
-        # Leg reawrd
-        leg_joint_angles = observation.joint_angles[[1,4,7,10,13,16,2,5,8,11,14,17]]
-        desired_leg_angles = self.retarget.retarget_joint_angle(dataloader.eval(now_t).q)[[[1,4,7,10,13,16,2,5,8,11,14,17]]]
-        diff = leg_joint_angles - desired_leg_angles
+        # Leg reawrd (waiting for the end effector reward to take the place of it)
+        leg_joints_angles = observation.joint_angles[[1,4,7,10,13,16,2,5,8,11,14,17]]
+        desired_leg_angles = sample.q[[1,4,7,10,13,16,2,5,8,11,14,17]]
+        diff = leg_joints_angles - desired_leg_angles
         weight_legs = params.get("weight_legs", 0)
         sigma_legs = params.get("sigma_legs", 0)
         leg_reward = weight_legs * np.exp(-np.sum(np.square(diff))/(2.0*12*sigma_legs**2))
 
+        # Joint velocities reward
+        joint_velocities = observation.joint_vel[list(self.mimic_joints_index)]
+        desired_velocities = sample.qdot[list(self.mimic_joints_index)]
+        diff = joint_velocities - desired_velocities
+        weight_joints_vel = params.get("weight_joints_vel", 0)
+        sigma_joints_vel = params.get("sigma_joints_vel", 0)
+        joints_vel_reward = weight_joints_vel * np.exp(-np.sum(np.square(diff))/(2.0*N_mimic*sigma_joints_vel**2))
 
         # =============
         # sum up rewards
         # =============
         smoothness_reward = params.get("weight_smoothness", 0) * (smoothness1_reward + smoothness2_reward)
-        reward = forward_vel_reward + smoothness_reward + torque_reward + height_reward + attitude_reward + joint_reward + leg_reward
+        reward = forward_vel_reward \
+                + smoothness_reward \
+                + height_reward     \
+                + root_ori_reward   \
+                + joints_reward     \
+                + leg_reward        \
+                + joints_vel_reward
 
         info = {
             "forward_vel_reward": forward_vel_reward,
             "height_reward": height_reward,
-            "attitude_reward": attitude_reward,
+            "root_ori_reward": root_ori_reward,
 
-            "torque_reward": torque_reward,
             "smoothness1_reward": smoothness1_reward,
             "smoothness2_reward": smoothness2_reward,
             "smoothness_reward": smoothness_reward,
 
-            "joint_reward": joint_reward,
+            "joints_reward": joints_reward,
+
+            "joints_vel_reward": joints_vel_reward,
 
             "leg_reward": leg_reward
         }
