4780506073a62361a5f5d9ed20b637acc58e9a5f
Alex-Marshall-Master <hujian@ethz.ch>
2023-05-19 18:04:38 +0200

    new version for visualize retargeting
    use member variables of env, (for example: eval_env.lerp, eval_env.phase ...)
    make this process more like the training process

--- src/python/retarget.py
+++ src/python/retarget.py
@@ -1,115 +1,16 @@
 import pathlib
 import datetime
 import time
+import gym
+from pylocogym.data.deep_mimic_bob_adapter import BobMotionBobAdapter, BobMotionDataFieldNames
 from pylocogym.data.deep_mimic_motion import DeepMimicMotion
 from pylocogym.data.lerp_dataset import LerpMotionDataset
+from pylocogym.data.loop_dataset import LoopKeyframeMotionDataset
 from stable_baselines3.common import utils
 from stable_baselines3.common.env_util import make_vec_env
 from stable_baselines3.common.vec_env import DummyVecEnv
 import numpy as np
 from scipy.spatial.transform import Rotation as R
-
-
- 
-class Retarget:
-    def __init__(self, action_shape, joint_lower_limit, joint_upper_limit, joint_default_angle = None):
-
-        self.joint_angle_limit_low = joint_lower_limit
-        self.joint_angle_limit_high = joint_upper_limit
-        self.joint_angle_default = joint_default_angle
-        if self.joint_angle_default is None:
-            self.joint_angle_default = np.array([ 0.  ,  0.  ,  0.  ,  0.  ,  0.  ,  0.  ,  0.  ,  0.  ,  0.  ,
-                                                0.  ,  0.  ,  0.  ,  0.  ,  0.  ,  0.  ,  0.  ,  0.  ,  0.  ,
-                                                0.  ,  0.  ,  0.  ,  0.  ,  0.  ,  0.  ,  0.  ,  0.  ,  0.  ,
-                                                0.  ,  0.  ,  0.  ,  0.  ,  0.  ,  0.  ,  0.  ,  0.  ,  0.  ,
-                                                0.  ,  0.  ,  0.  ,  0.  ,  0.  ,  0.  ,  0.  ,  0 ])
-        self.joint_scale_factors = np.maximum(abs(self.joint_angle_default - self.joint_angle_limit_low),
-                                abs(self.joint_angle_default - self.joint_angle_limit_high))
-        self.action_shape = action_shape
-        
-    def quart_to_rpy(self, q, mode):
-        # q is in (w,x,y,z) format
-        q_xyzw = list(q[1:])
-        q_xyzw.append(q[0])
-        r = R.from_quat(q_xyzw) 
-        euler = r.as_euler(mode)
-        return euler[0], euler[1], euler[2]
-    
-    def rescale_action(self, action):
-        bound_action = np.minimum(np.maximum(action,self.joint_angle_limit_low),self.joint_angle_limit_high)
-        scaled_action = (bound_action - self.joint_angle_default) / self.joint_scale_factors 
-        return scaled_action
-    
-    def retarget_base_orientation(self, motion_clips_q):
-        (yaw, pitch, roll)  = self.quart_to_rpy(motion_clips_q[3:7], 'yzx')
-        return yaw, -pitch, roll
-
-    def retarget_joint_angle(self, motion_clips_q, require_root = False):
-        """Given a motion_clips orientation data, return a retarget action"""
-        action = np.zeros(self.action_shape)
-        if not require_root:
-
-            (chest_z, chest_y, chest_x) = self.quart_to_rpy(motion_clips_q[7:11], 'zyx')
-            (neck_z,  neck_y,  neck_x) = self.quart_to_rpy(motion_clips_q[11:15],'zyx')
-            (r_hip_z, r_hip_x, r_hip_y) = self.quart_to_rpy(motion_clips_q[15:19],'zxy')
-            (r_ankle_z, r_ankle_x, r_ankle_y) = self.quart_to_rpy(motion_clips_q[20:24],'zxy')
-            (r_shoulder_z, r_shoulder_x, r_shoulder_y) = self.quart_to_rpy(motion_clips_q[24:28],'zxy')
-            (l_hip_z, l_hip_x, l_hip_y) = self.quart_to_rpy(motion_clips_q[29:33],'zxy')
-            (l_ankle_z, l_ankle_x, l_ankle_y) = self.quart_to_rpy(motion_clips_q[34:38],'zxy')
-            (l_shoulder_z, l_shoulder_x, l_shoulder_y) = self.quart_to_rpy(motion_clips_q[38:42],'zxy')
-
-            # chest - xyz euler angle 
-            action[0] = -chest_z
-            action[3] = chest_y
-            action[6] = chest_x
-
-            # neck - xyz euler angle 
-            action[18] = -neck_z
-            action[23] = neck_y
-            action[26] = neck_x
-
-            # shoulder - xzy euler angle 
-            action[27] = -l_shoulder_z
-            action[30] = l_shoulder_x
-            action[33] = l_shoulder_y
-
-            action[28] = -r_shoulder_z
-            action[31] = r_shoulder_x
-            action[34] = r_shoulder_y
-
-            # ankle - xzy euler angle 
-            action[13] = -l_ankle_z
-            action[16] = l_ankle_x
-
-            action[14] = -r_ankle_z
-            action[17] = r_ankle_x            
-
-            # hip - xzy euler angle 
-            action[1] = -l_hip_z
-            action[4] = l_hip_x
-            action[7] = l_hip_y
-
-            action[2] = -r_hip_z
-            action[5] = r_hip_x
-            action[8] = r_hip_y
-
-            r_knee = motion_clips_q[19:20]
-            r_elbow = motion_clips_q[28:29]
-            l_knee = motion_clips_q[33:34]
-            l_elbow = motion_clips_q[42:43]
-
-            # elbow - revolute joint 
-            action[36] = l_elbow
-            action[37] = r_elbow
-
-            # knee - revolute joint 
-            action[10] = l_knee
-            action[11] = r_knee
-
-            action = self.rescale_action(action)
-
-        return action
-
 
 
 def test(params, reward_path=None, data_path = None):
@@ -130,63 +31,28 @@ def test(params, reward_path=None, data_path = None):
 
     if reward_path is not None:
         reward_params["reward_file_path"] = reward_path  # add reward path to reward params
-
-    # =============
-    # load motion clips data
-    # =============
-
-    if data_path is not None:
-        dataset = DeepMimicMotion(data_path, 0)
-        lerp = LerpMotionDataset(dataset)
-
-    # =============
-    # create a single environment for evaluation
-    # =============
-
-    eval_env = make_vec_env(
-        env_id,
-        n_envs=1,
-        seed=seed,
-        env_kwargs=env_kwargs,
-        vec_env_cls=DummyVecEnv,
-    )
-
-    # =============
-    # joint config 
-    # =============
-
-    joint_lower_limit = np.array([-0.5 , -1.2 , -1.2 , -0.5 , -0.5 , -1.2 , -0.5 , -0.75, -0.75,
-                                -0.5 ,  0.  ,  0.  , -0.5 , -0.5 , -0.5 , -0.5 , -0.5 , -0.5 ,
-                                -0.5 , -0.8 , -0.2 , -0.75, -0.75, -0.5 , -0.2 , -0.6 , -0.5 ,
-                                -3.  , -3.  , -0.5 , -0.3 , -1.2 , -0.5 , -0.75, -0.5 , -0.5 ,
-                                -2.5 , -2.5 , -1.  , -1.6 , -0.5 , -0.5 , -1.  , -1.  ])
-    joint_upper_limit = np.array([0.5 , 1.2 , 1.2 , 0.5 , 1.2 , 0.5 , 0.5 , 0.75, 0.75, 0.5 , 2.,
-                                2.  , 0.5 , 0.9 , 0.9 , 0.5 , 0.5 , 0.5 , 0.5 , 0.2 , 0.8 , 0.2 ,
-                                0.2 , 0.5 , 0.6 , 0.2 , 0.5 , 0.5 , 0.5 , 0.5 , 1.2 , 0.3 , 0.5 ,
-                                1.  , 0.75, 0.5 , 0.  , 0.  , 1.6 , 1.  , 0.5 , 0.5 , 1.  , 1.  ])
     
+    # =============
+    # create a simple environment for evaluation
+    # =============
+
+    eval_env = gym.make(env_id, **env_kwargs)
     
     # =============
     # start playing
     # =============
     episodes = 100
-    frame_rate = 60
-    action_shape = eval_env.action_space.shape[0] 
-    retarget=Retarget(action_shape,joint_lower_limit,joint_upper_limit)
-
+    frame_rate = 30
+    
     for ep in range(episodes):
         eval_env.reset()
         done = False
-        t = 0
+        t = eval_env.phase*eval_env.dataset.duration
         while not done:
             eval_env.render("human")
-            action = retarget.retarget_joint_angle(lerp.eval(t).q)
-            (desired_yaw, desired_pitch, desired_roll) = retarget.retarget_base_orientation(lerp.eval(t).q)
-            obs, reward, done, info = eval_env.step([action])
-            print(desired_yaw, desired_pitch, desired_roll, "==" ,obs[0,3], obs[0,4], obs[0,5])
+            action = eval_env.lerp.eval(t).q[6:]
+            obs, reward, done, info = eval_env.step(action)
+            print("now time, now phase", obs[0,-2],obs[0,-1])
             t += 1.0/frame_rate
             # time.sleep(0.1)
-        lerp.reset()
-       
-            
     eval_env.close()
