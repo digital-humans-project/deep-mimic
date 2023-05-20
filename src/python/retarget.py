2167b3fd27a8bcd0612574573ed079e9c8f2c880
Alex-Marshall-Master <hujian@ethz.ch>
2023-05-20 13:24:23 +0200

    test multi envs

--- src/python/retarget.py
+++ src/python/retarget.py
@@ -36,23 +36,38 @@ def test(params, reward_path=None, data_path = None):
     # create a simple environment for evaluation
     # =============
 
-    eval_env = gym.make(env_id, **env_kwargs)
+    # eval_env = gym.make(env_id, **env_kwargs)
+
+    eval_env = make_vec_env(
+        env_id,
+        n_envs=2,
+        seed=seed,
+        env_kwargs=env_kwargs,
+        vec_env_cls=DummyVecEnv
+    )
     
     # =============
     # start playing
     # =============
     episodes = 100
     frame_rate = 30
-    
+
     for ep in range(episodes):
         eval_env.reset()
-        done = False
-        t = eval_env.phase*eval_env.dataset.duration
-        while not done:
-            eval_env.render("human")
-            action = eval_env.lerp.eval(t).q[6:]
-            obs, reward, done, info = eval_env.step(action)
-            print("now time, now phase", obs[0,-2],obs[0,-1])
-            t += 1.0/frame_rate
+        done1 = False
+        done2 = False
+        t1 = eval_env.envs[0].phase*eval_env.envs[0].dataset.duration
+        t2 = eval_env.envs[1].phase*eval_env.envs[1].dataset.duration
+        while not done1 and not done2:
+            eval_env.envs[0].render("human")
+            action = eval_env.envs[0].lerp.eval(t1).q[6:]
+            obs, reward, done1, info = eval_env.envs[0].step(action)
+            print("now time, now phase", obs[-2],obs[-1])
+
+            action2 = eval_env.envs[1].lerp.eval(t2).q[6:]
+            _,_,done2,_ = eval_env.envs[1].step(action2)
+
+            t1 += 1.0/frame_rate
+            t2 += 1.0/frame_rate
             # time.sleep(0.1)
     eval_env.close()
