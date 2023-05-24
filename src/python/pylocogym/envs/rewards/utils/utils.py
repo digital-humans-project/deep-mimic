80d90d545b3060cbbdab09e2182e0e45788feac2
Alex-Marshall-Master <hujian@ethz.ch>
2023-05-24 23:24:13 +0200

    new reward for observation

--- src/python/pylocogym/envs/rewards/utils/utils.py
+++ src/python/pylocogym/envs/rewards/utils/utils.py
@@ -27,7 +27,7 @@ class ObservationData:
             self.z = observation_raw[2]
 
             # orientation:
-            # self.ori = observation_raw[3:6]  # Euler angles (yaw, pitch, roll)
+            self.ori = observation_raw[3:6]  # Euler angles (yaw, pitch, roll)
             # self.yaw = observation_raw[3]
             # self.pitch = observation_raw[4]
             # self.roll = observation_raw[5]
@@ -35,13 +35,14 @@ class ObservationData:
             self.ori_y = observation_raw[4]
             self.ori_z = observation_raw[5]
             self.ori_w = np.sqrt(1 - np.sum(np.square(observation_raw[3:6])))
+            self.ori_q = np.array([self.ori_x,self.ori_y,self.ori_z,self.ori_w])
 
             # joint angles:
             self.joint_angles = observation_raw[6:6 + num_joints]
 
             # linear velocity:
             self.vel = observation_raw[6 + num_joints:9 + num_joints]  # in global coordinates
-            rotation_to_local = Rotation.from_euler('yxz', -self.ori)
+            rotation_to_local = Rotation.from_euler('YXZ', -self.ori)
             self.local_vel = rotation_to_local.apply(self.vel)
 
             # angular velocity:
