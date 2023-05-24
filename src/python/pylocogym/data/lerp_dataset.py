69574c739241a3707fd3c8dd021a80a498b19144
Howyoung <howyoung3@gmail.com>
2023-05-24 18:06:41 +0200

    feat: slerp in lerp dataset

--- src/python/pylocogym/data/lerp_dataset.py
+++ src/python/pylocogym/data/lerp_dataset.py
@@ -10,9 +10,12 @@ except ImportError:
         return zip(a, b)
 
 
-from typing import Optional
+from enum import Enum, StrEnum
+from typing import List, Optional, Tuple
 
-from pylocogym.data.dataset import IterableKeyframeMotionDataset, MotionDataSample
+import numpy as np
+
+from pylocogym.data.dataset import IterableKeyframeMotionDataset, KeyframeMotionDataSample, MotionDataSample
 
 
 class ContinuousMotionDataset:
@@ -45,22 +48,85 @@ def lerp(a, b, alpha):
     return a * (1 - alpha) + b * alpha
 
 
+def normalize(v):
+    return v / np.linalg.norm(v)
+
+
+def quaternion_slerp(
+    quat0,
+    quat1,
+    fraction,
+    spin=0,
+    shortest_path=True,
+    eps=np.finfo(float).eps * 4.0,
+):
+    """Return spherical linear interpolation between two quaternions.
+
+    >>> q0 = random_quaternion()
+    >>> q1 = random_quaternion()
+    >>> q = quaternion_slerp(q0, q1, 0)
+    >>> numpy.allclose(q, q0)
+    True
+    >>> q = quaternion_slerp(q0, q1, 1, 1)
+    >>> numpy.allclose(q, q1)
+    True
+    >>> q = quaternion_slerp(q0, q1, 0.5)
+    >>> angle = math.acos(numpy.dot(q0, q))
+    >>> numpy.allclose(2, math.acos(numpy.dot(q0, q1)) / angle) or \
+        numpy.allclose(2, math.acos(-numpy.dot(q0, q1)) / angle)
+    True
+
+    """
+    q0 = normalize(quat0[:4])
+    q1 = normalize(quat1[:4])
+    if fraction == 0.0:
+        return q0
+    elif fraction == 1.0:
+        return q1
+    d = np.dot(q0, q1)
+    if abs(abs(d) - 1.0) < eps:
+        return q0
+    if shortest_path and d < 0.0:
+        # invert rotation
+        d = -d
+        q1 = -q1
+    angle = np.arccos(d) + spin * np.pi
+    if abs(angle) < eps:
+        return q0
+    isin = 1.0 / np.sin(angle)
+    q0 *= np.sin((1.0 - fraction) * angle) * isin
+    q1 *= np.sin(fraction * angle) * isin
+    q0 += q1
+    return q0
+
+
 class LerpMotionDataset(ContinuousMotionDataset):
     """
     `LerpMotionDataset` is a wrapper around `IterableKeyframeMotionDataset` that
     interpolates between keyframes using linear interpolation.
     """
 
-    def __init__(self, kf_dataset: IterableKeyframeMotionDataset) -> None:
+    def __init__(
+        self,
+        kf_dataset: IterableKeyframeMotionDataset,
+        lerp_fields: List[StrEnum] = [],
+        slerp_fields: List[StrEnum] = [],
+        slerp_spin: int = 0,
+        slerp_shortest_path: bool = True,
+    ) -> None:
         super().__init__(kf_dataset)
+        self.lerp_fields = lerp_fields
+        self.slerp_fields = slerp_fields
         self.kf_dataset = kf_dataset
+        self.slerp_spin = slerp_spin
+        self.slerp_shortest_path = slerp_shortest_path
         self.reset()
 
     def reset(self) -> None:
         self.kf_iter = iter(self.kf_dataset)
         self.cur_kf = next(self.kf_iter)
 
-    def eval(self, t: float) -> Optional[MotionDataSample]:
+    def eval(self, t: float) -> Optional[Tuple[MotionDataSample, KeyframeMotionDataSample]]:
         assert (
             t >= self.cur_kf.t0
         ), f"t = {t} < time of current keyframe {self.cur_kf.t0}, t must be monotonically increasing"
@@ -71,6 +137,16 @@ class LerpMotionDataset(ContinuousMotionDataset):
                 return None
         kf = self.cur_kf
         alpha = (t - kf.t0) / kf.dt
-        q = lerp(kf.q0, kf.q1, alpha)
         phase = lerp(kf.phase0, kf.phase1, alpha)
-        return self.kf_dataset.SampleType.BaseSampleType(t, q, kf.qdot, phase)
+        sample = self.kf_dataset.SampleType.BaseSampleType(t, kf.q0, kf.qdot, phase)
+        for field in self.lerp_fields:
+            sample.q_fields[field] = lerp(kf.q0_fields[field], kf.q1_fields[field], alpha)
+        for field in self.slerp_fields:
+            sample.q_fields[field] = quaternion_slerp(
+                kf.q0_fields[field],
+                kf.q1_fields[field],
+                alpha,
+                spin=self.slerp_spin,
+                shortest_path=self.slerp_shortest_path,
+            )
+        return sample, kf
