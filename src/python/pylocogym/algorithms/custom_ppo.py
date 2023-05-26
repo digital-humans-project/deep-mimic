5e36bedb3796ccac24f4b5e13ae7ab550edd9a00
Howyoung <howyoung3@gmail.com>
2023-05-26 20:52:22 +0200

    fix: unpicklable func

--- src/python/pylocogym/algorithms/custom_ppo.py
+++ src/python/pylocogym/algorithms/custom_ppo.py
@@ -1,6 +1,6 @@
 # for testing
 # added collect / train time log
-from typing import Optional
+from typing import Optional, Union
 from gym import spaces
 import numpy as np
 import time
@@ -8,25 +8,51 @@ import torch as th
 from torch.nn import functional as F
 
 from stable_baselines3 import PPO
-from stable_baselines3.common.type_aliases import GymEnv, MaybeCallback
-from stable_baselines3.common.utils import safe_mean, explained_variance, get_schedule_fn
+from stable_baselines3.common.type_aliases import GymEnv, MaybeCallback, Schedule
+from stable_baselines3.common.utils import safe_mean, explained_variance
+
+
+class ConstantLrSchedule:
+    def __init__(self, value) -> None:
+        self.value = float(value)
+
+    def __call__(self, progress_remaining):
+        return self.value
+
+
+def get_schedule_fn(value_schedule: Union[Schedule, float, int]) -> Schedule:
+    """
+    Transform (if needed) learning rate and clip range (for PPO)
+    to callable.
+
+    :param value_schedule:
+    :return:
+    """
+    # If the passed schedule is a float
+    # create a constant function
+    if isinstance(value_schedule, (float, int)):
+        # Cast to float to avoid errors
+        value_schedule = ConstantLrSchedule(value_schedule)
+    else:
+        assert callable(value_schedule)
+    return value_schedule
 
 
 class CustomPPO(PPO):
     def __init__(
-            self,
-            policy,
-            env,
-            learning_rate,
-            batch_size,
-            n_epochs,
-            n_steps,
-            vf_coef,
-            gamma, 
-            gae_lambda, 
-            clip_range,
-            policy_kwargs,
-            seed,
+        self,
+        policy,
+        env,
+        learning_rate,
+        batch_size,
+        n_epochs,
+        n_steps,
+        vf_coef,
+        gamma,
+        gae_lambda,
+        clip_range,
+        policy_kwargs,
+        seed,
     ):
         super().__init__(
             policy=policy,
@@ -35,9 +61,9 @@ class CustomPPO(PPO):
             batch_size=batch_size,
             n_epochs=n_epochs,
             n_steps=n_steps,
-            gamma = gamma,
-            gae_lambda = gae_lambda,
-            clip_range = clip_range,
+            gamma=gamma,
+            gae_lambda=gae_lambda,
+            clip_range=clip_range,
             vf_coef=vf_coef,
             policy_kwargs=policy_kwargs,
             seed=seed,
@@ -187,29 +213,37 @@ class CustomPPO(PPO):
         self.logger.record("time/evaluate_actions_time", np.mean(evaluate_actions_time))
 
     def learn(
-            self,
-            total_timesteps: int,
-            callback: MaybeCallback = None,
-            log_interval: int = 1,
-            eval_env: Optional[GymEnv] = None,
-            eval_freq: int = -1,
-            n_eval_episodes: int = 5,
-            tb_log_name: str = "PPO",
-            eval_log_path: Optional[str] = None,
-            reset_num_timesteps: bool = True,
+        self,
+        total_timesteps: int,
+        callback: MaybeCallback = None,
+        log_interval: int = 1,
+        eval_env: Optional[GymEnv] = None,
+        eval_freq: int = -1,
+        n_eval_episodes: int = 5,
+        tb_log_name: str = "PPO",
+        eval_log_path: Optional[str] = None,
+        reset_num_timesteps: bool = True,
     ):
         iteration = 0
 
         total_timesteps, callback = self._setup_learn(
-            total_timesteps, eval_env, callback, eval_freq, n_eval_episodes, eval_log_path, reset_num_timesteps, tb_log_name
+            total_timesteps,
+            eval_env,
+            callback,
+            eval_freq,
+            n_eval_episodes,
+            eval_log_path,
+            reset_num_timesteps,
+            tb_log_name,
         )
 
         callback.on_training_start(locals(), globals())
 
         while self.num_timesteps < total_timesteps:
-
             start = time.time()
-            continue_training = self.collect_rollouts(self.env, callback, self.rollout_buffer, n_rollout_steps=self.n_steps)
+            continue_training = self.collect_rollouts(
+                self.env, callback, self.rollout_buffer, n_rollout_steps=self.n_steps
+            )
             end = time.time()
 
             if continue_training is False:
@@ -223,8 +257,12 @@ class CustomPPO(PPO):
                 fps = int((self.num_timesteps - self._num_timesteps_at_start) / (time.time() - self.start_time))
                 self.logger.record("time/iterations", iteration, exclude="tensorboard")
                 if len(self.ep_info_buffer) > 0 and len(self.ep_info_buffer[0]) > 0:
-                    self.logger.record("rollout/ep_rew_mean", safe_mean([ep_info["r"] for ep_info in self.ep_info_buffer]))
-                    self.logger.record("rollout/ep_len_mean", safe_mean([ep_info["l"] for ep_info in self.ep_info_buffer]))
+                    self.logger.record(
+                        "rollout/ep_rew_mean", safe_mean([ep_info["r"] for ep_info in self.ep_info_buffer])
+                    )
+                    self.logger.record(
+                        "rollout/ep_len_mean", safe_mean([ep_info["l"] for ep_info in self.ep_info_buffer])
+                    )
                 self.logger.record("time/fps", fps)
                 self.logger.record("time/time_elapsed", int(time.time() - self.start_time), exclude="tensorboard")
                 self.logger.record("time/total_timesteps", self.num_timesteps, exclude="tensorboard")
