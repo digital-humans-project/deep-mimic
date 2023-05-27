da362482e9fed225e1978fb186a477ed29000eaf
Howyoung <howyoung3@gmail.com>
2023-05-27 23:33:03 +0200

    job: crawl

--- jobs/deep_mimic_crawl.sh
+++ jobs/deep_mimic_crawl.sh
@@ -4,11 +4,11 @@
 #SBATCH --time=24:00:00                    # hours:minutes:seconds
 #SBATCH --mem-per-cpu=2000
 #SBATCH --tmp=4000                        # per node!!
-#SBATCH --job-name=deep_mimic_jog
-#SBATCH --output=./out/deep_mimic_jog.out
-#SBATCH --error=./out/deep_mimic_jog.err
+#SBATCH --job-name=deep_mimic_crawl
+#SBATCH --output=./out/deep_mimic_crawl.out
+#SBATCH --error=./out/deep_mimic_crawl.err
 #SBATCH --gpus=1
 
 source scripts/setup.sh
 
-xvfb-run -a --server-args="-screen 0 480x480x24" python src/python/main.py -wb -vr -c bob_env_jog.json
+xvfb-run -a --server-args="-screen 0 480x480x24" python src/python/main.py -wb -vr -c bob_env_crawl.json
