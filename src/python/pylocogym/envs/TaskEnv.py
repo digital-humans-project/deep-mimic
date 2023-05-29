c98b0505db85a02ec499ac94f64a499702ab271c
alex-marshall-euler <hujian@ethz.ch>
2023-05-29 20:25:15 +0200

    action

--- src/python/pylocogym/envs/TaskEnv.py
+++ src/python/pylocogym/envs/TaskEnv.py
@@ -146,7 +146,7 @@ class TaskEnv(VanillaEnv):
 
         # run simulation
         action_applied = self.scale_action(action)
-        self._sim.step(action)
+        self._sim.step(action_applied)
         observation = self.get_obs()
 
         # update variables
