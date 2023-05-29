312ea9a4ea58ca68cb8917527bea1190cb1f4dfa
Howyoung <howyoung3@gmail.com>
2023-05-29 17:02:26 +0200

    job: crawl residual

---
+++ jobs/deep_mimic_crawl_residual.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#SBATCH -n 20                              # Number of cores
+#SBATCH --time=24:00:00                    # hours:minutes:seconds
+#SBATCH --mem-per-cpu=2000
+#SBATCH --tmp=4000                        # per node!!
+#SBATCH --job-name=deep_mimic_crawl_residual
+#SBATCH --output=./out/deep_mimic_crawl_residual.out
+#SBATCH --error=./out/deep_mimic_crawl_residual.err
+#SBATCH --gpus=1
+
+source scripts/setup.sh
+
+xvfb-run -a --server-args="-screen 0 480x480x24" python src/python/main.py -wb -vr -c bob_env_crawl_residual.json
