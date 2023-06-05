320322fd6a4f7c02cb100ccfee4537de464797fe
Ankita Ghosh <ankitaghosh0907@gmail.com>
2023-06-05 13:12:53 +0200

    New env for multiclip training

--- src/python/pylocogym/envs/__init__.py
+++ src/python/pylocogym/envs/__init__.py
@@ -1,6 +1,8 @@
 from gym.envs.registration import register
 from .VanillaEnv import VanillaEnv
 from .TaskEnv import TaskEnv
+from .MultiClipEnv import MultiClipEnv
 
 register(id="PylocoVanilla-v0", entry_point=VanillaEnv)
 register(id="PylocoVanillaTask-v0", entry_point=TaskEnv)
+register(id="PylocoMultiClip-v0", entry_point=MultiClipEnv)
