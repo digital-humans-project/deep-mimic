bc755ab1a358681781a8f193ed3b82bdcb4bf232
Alex-Marshall-Master <hujian@ethz.ch>
2023-06-14 17:41:05 +0200

    prepare for merge

--- src/python/pylocogym/envs/__init__.py
+++ src/python/pylocogym/envs/__init__.py
@@ -1,4 +1,6 @@
 from gym.envs.registration import register
 from .VanillaEnv import VanillaEnv
+from .ResidualEnv import ResidualEnv
 
 register(id="PylocoVanilla-v0", entry_point=VanillaEnv)
+register(id="PylocoResidual-v0", entry_point=ResidualEnv)
